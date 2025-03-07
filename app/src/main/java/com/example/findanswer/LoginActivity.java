98d13d25328c656b08de175599f7018ea32cf7f9
Alen9136 <dovevan@list.ru>
2025-03-07 23:51:46 +0400

    add a 1 page for sign up and sing in

---
+++ app/src/main/java/com/example/findanswer/LoginActivity.java
@@ -0,0 +1,5 @@
+package com.example.findanswer;
+
+public class LoginActivity {
+
+}
---
+++ app/src/main/java/com/example/findanswer/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.findanswer;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView loginLink = findViewById(R.id.btnLogin);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
